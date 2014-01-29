e3658fa568147f687c66ce27eb888f0e4a3c24c5
Ruohonen Henna M <hennaruo@da1-cs-bk107-36.helsinki.fi>
2014-01-29 12:19:49 +0200

    käyttöliittymäpakkaus

---
+++ valumaohjelma/src/main/java/kayttoliittyma/Tekstikayttoliittyma.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kayttoliittyma;
+
+import valuma.logiikka.Paivittaja;
+
+/**
+ *
+ * @author hennaruo@cs
+ */
+public class Tekstikayttoliittyma {
+    private Paivittaja maailmanluoja;
+
+    public Tekstikayttoliittyma() {
+        maailmanluoja = new Paivittaja(20);
+    }
+    
+    
+
+    
+    
+    
+}
