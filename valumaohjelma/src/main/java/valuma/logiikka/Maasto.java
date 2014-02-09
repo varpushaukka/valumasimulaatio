c0eceee166f94e9c78dcf8f4aed639fece6d51d0
Henna Ruohonen <varpushaukka@oanlupe.(none)>
2014-02-09 11:41:16 +0200

    hieno on

--- valumaohjelma/src/main/java/valuma/logiikka/Maasto.java
+++ valumaohjelma/src/main/java/valuma/logiikka/Maasto.java
@@ -31,8 +31,8 @@ public class Maasto {
         for (int i = 0; i < koko; i++) {
             for (int j = 0; j < koko; j++) {
 ////                setMaankorkeus(i, j, 10 + rnd.nextFloat());
-//                setMaankorkeus(i, j, 10 + (koko - i) / 15 + (koko - j) / 15);
-                setMaankorkeus(i, j, 11);
+                setMaankorkeus(i, j, 10 + (koko - i) / 15 + (koko - j) / 15);
+//                setMaankorkeus(i, j, 11);
 
             }
         }
